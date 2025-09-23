3589c5085cd05a8ffe9fcc6187ab0e6dcabf3170
SparkyBeles <beles.dev@gmail.com>
2025-09-23 15:50:27 +0200

    raouter setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Analytics } from "@vercel/analytics/react"
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from "./App";
+import { Analytics } from "@vercel/analytics/react";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -10,3 +11,4 @@ createRoot(document.getElementById('root')).render(
     <Analytics/>
   </StrictMode>,
 )
+ 
