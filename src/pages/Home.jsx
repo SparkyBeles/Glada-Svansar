c2b9ece9026d9cfd7dbe8b5a0c89da48d1712d84
SparkyBeles <beles.dev@gmail.com>
2025-09-23 20:08:33 +0200

    second draft  design for Home & about page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,10 @@ import "../style/HomeStyle.css";
 function Home(){
     return(
         <div className="background">
-
+           <div className="hero"> 
+            <h1>Glada Svansar </h1>
+            </div>
+           
 
         </div>
 
