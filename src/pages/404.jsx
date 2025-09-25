647289617bdaea9907fa5cc9edc09877de18efc3
SparkyBeles <beles.dev@gmail.com>
2025-09-25 12:55:50 +0200

    404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,9 @@
+import "../style/404.css";
+function Page404() {
+  return (
+    <div className="background-404">
+      <h1 className="h1-404">Sidan kunde inte hittas...!</h1>
+    </div>
+  );
+}
+export default Page404;
